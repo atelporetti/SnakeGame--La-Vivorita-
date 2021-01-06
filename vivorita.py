5e7b526c20f20e7b029892c86632228beac760cc
atelito <aaxelporetti@gmail.com>
2021-01-06 12:36:15 -0300

    mensaje settings ok

--- vivorita.py
+++ vivorita.py
@@ -42,31 +42,35 @@ frame.columnconfigure((0, columnas), weight=1)
 scroll = Scrollbar(raiz, command=frame)
 scroll.grid(row=0, column=3, sticky='nsew') 
 
-bienvenida_img = Label(frame, image=imagen_bienvenida)
-bienvenida_img.grid(row=0, column=0, columnspan=2,
+lb_bienvenida_img = Label(frame, image=imagen_bienvenida)
+lb_bienvenida_img.grid(row=0, column=0, columnspan=2,
                     sticky='nsew', padx=10, pady=10)
 
-bienvenida = Label(frame, text='La Vivorita: el juego',
+lb_bienvenida = Label(frame, text='La Vivorita: el juego',
                    font=("256 Bytes", 40), bg='black', fg='white')
 # RitzFLF, 256 Bytes, RightBankFLF, Franchise,
-bienvenida.grid(row=1, column=0, columnspan=3, sticky='nsew', padx=10, pady=10)
-bienvenida.rowconfigure((0, filas), weight=1)
-bienvenida.columnconfigure((0, columnas), weight=1)
+lb_bienvenida.grid(row=1, column=0, columnspan=3, sticky='nsew', padx=10, pady=10)
+lb_bienvenida.rowconfigure((0, filas), weight=1)
+lb_bienvenida.columnconfigure((0, columnas), weight=1)
 
 
 nombre = StringVar()
 
-nombre_j = Label(frame, text='Nombre Jugador:')
-nombre_j.config(bg='black', fg='white', font=("RitzFLF", 12))
-nombre_j.grid(row=2, column=0, sticky='nsew', padx=10, pady=10)
-nombre_jugador = Entry(frame, textvariable=nombre)
-nombre_jugador.grid(row=2, column=1, sticky='nsew', padx=10, pady=10)
-nombre_jugador.config(bg='black', fg='#B2BD08',
+lb_nombre_j = Label(frame, text='Nombre Jugador:')
+lb_nombre_j.config(bg='black', fg='white', font=("RitzFLF", 12))
+lb_nombre_j.grid(row=2, column=0, sticky='nsew', padx=10, pady=10)
+lb_nombre_jugador = Entry(frame, textvariable=nombre)
+lb_nombre_jugador.grid(row=2, column=1, sticky='nsew', padx=10, pady=10)
+lb_nombre_jugador.config(bg='black', fg='#B2BD08',
                       justify='center', font=("RitzFLF", 12))
 
+msg_alerta = Message(frame)
+msg_alerta.grid(row=4, column=0, sticky='nsew', padx=10, pady=10)
+msg_alerta.config(bg='black', fg='red', justify='left', anchor=CENTER, font=("RitzFLF", 12))
 
-#nom = Label(frame, textvariable=nombre)
-#nom.grid(row=3, column=0, sticky='nsew', padx=10, pady=10)
+def msj_alerta(msj):
+    msg_alerta.config(text=str(msj))
+
 """
 def play():
     pygame.mixer.music.load('audio\snake.mp3')
@@ -108,7 +112,7 @@ def cambio_musica():
 btn_musica_on_off = Button(
     frame, text=txt_musica_on_off, command=cambio_musica)
 
-btn_musica_on_off.grid(row=3, column=1, sticky='nsew', padx=10, pady=10)
+btn_musica_on_off.grid(row=4, column=1, sticky='nsew', padx=10, pady=10)
 btn_musica_on_off.config(bg='black', fg='white',
                          justify='center', font=("RitzFLF", 12))
 
@@ -117,50 +121,48 @@ velocidad = 0
 dificultad = IntVar()
 
 def cambia_dificultad():
-    if dificultad.get() == 0:
+    if dificultad.get() == 1:
         velocidad = 1
-        vel.config(text='Facil')
-        facil['fg'] = '#B2BD08'
-        dificil['fg'] = 'white'
-    elif dificultad.get() == 1:
+        rb_facil['fg'] = '#B2BD08'
+        rb_dificil['fg'] = 'white'
+    elif dificultad.get() == 2:
         velocidad = 2
-        vel.config(text='Dificil')
-        dificil['fg'] = '#B2BD08'
-        facil['fg'] = 'white'
+        rb_dificil['fg'] = '#B2BD08'
+        rb_facil['fg'] = 'white'
 
-
-def guarda_nombre():
-    if (nombre.get() == '' and dificultad.get() != (0 or 1)):
-        print('Elija un nombre y nivel de dificultad')
-    elif (nombre.get() == ''):
-        print('Elija un nombre')
-    elif (dificultad.get() != (0 or 1)):
-        print('Elija la dificultad')
+def guarda_datos():
+    alerta = ''
+    if (nombre.get() == '' and dificultad.get() == 0):
+        alerta = 'Elija un nombre y nivel de dificultad'
+        msj_alerta(alerta)
+        return print(alerta)
+    if (nombre.get() == ''):
+        alerta = 'Elija un nombre'
+        msj_alerta(alerta)
+        return print(alerta)
+    if (dificultad.get() == 0):
+        alerta = 'Elija la dificultad'
+        msj_alerta(alerta)
+        return print(alerta)
     elif (nombre.get() and dificultad.get()):
         print('Comenzando juego...')
+        msj_alerta(alerta)
         return nombre.get(), dificultad.get()
 
 
-facil = Radiobutton(frame, text='Fácil', variable=dificultad,
-                    value=0, command=cambia_dificultad)
-facil.config(bg='black', fg='white', justify='center', font=("RitzFLF", 12))
-facil.grid(row=4, column=0, sticky='nsew', padx=10, pady=10)
-dificil = Radiobutton(frame, text='Dificil',
-                      variable=dificultad, value=1, command=cambia_dificultad)
-dificil.config(bg='black', fg='white', justify='center', font=("RitzFLF", 12))
-dificil.grid(row=4, column=1, sticky='nsew', padx=10, pady=10)
+rb_facil = Radiobutton(frame, text='Fácil', variable=dificultad, value=1, command=cambia_dificultad)
+rb_facil.config(bg='black', fg='white', justify='center', font=("RitzFLF", 12))
+rb_facil.grid(row=3, column=0, sticky='nsew', padx=10, pady=10)
+rb_dificil = Radiobutton(frame, text='Dificil', variable=dificultad, value=2, command=cambia_dificultad)
+rb_dificil.config(bg='black', fg='white', justify='center', font=("RitzFLF", 12))
+rb_dificil.grid(row=3, column=1, sticky='nsew', padx=10, pady=10)
 
-vel = Label(frame)
-vel.grid(row=5, column=1, sticky='nsew', padx=10, pady=10)
-vel.config(bg='black', fg='white', justify='center', font=("RitzFLF", 12))
-
-
-jugar = Button(frame, text='JUGAR')
-jugar.grid(row=5, column=0, columnspan=2, sticky='nsew', padx=10, pady=10)
-jugar.config(bg='black', fg='#B2BD08', justify='center',
-             font=("256 Bytes", 20), command=guarda_nombre)
-jugar.rowconfigure((0, filas), weight=1)
-jugar.columnconfigure((0, columnas), weight=1)
+btn_jugar = Button(frame, text='JUGAR')
+btn_jugar.grid(row=5, column=0, columnspan=2, sticky='nsew', padx=10, pady=10)
+btn_jugar.config(bg='black', fg='#B2BD08', justify='center',
+             font=("256 Bytes", 20), command=guarda_datos)
+btn_jugar.rowconfigure((0, filas), weight=1)
+btn_jugar.columnconfigure((0, columnas), weight=1)
 
 
 raiz.mainloop()
